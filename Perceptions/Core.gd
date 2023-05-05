9c9485a88fab2a20fb1db2096116e31666d39341
mk56-spn <darioheadleysilva@gmail.com>
2023-05-05 16:26:33 +0200

    Change the physics to better represent the desired jump height, as well as changing the jump force to account for this change

--- Perceptions/Core.gd
+++ Perceptions/Core.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const JUMP_VELOCITY = -1600.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity: int = ProjectSettings.get_setting("physics/2d/default_gravity")
