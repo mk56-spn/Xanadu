37b6b50f75e78258f5d463dab561696f9a126fc4
mk56-spn <darioheadleysilva@gmail.com>
2023-06-25 15:52:11 +0200

    Add new level selector scene and script.

---
+++ Screens/StageSelection.cs
@@ -0,0 +1,15 @@
+using Godot;
+
+namespace XanaduProject.Screens;
+
+public partial class StageSelection : Control
+{
+    public override void _Ready()
+    {
+        base._Ready();
+
+        GetNode<Button>("HBoxContainer/Button").ButtonDown += () => GetTree().ChangeSceneToFile("res://Stages/TestStage.tscn");
+        GetNode<Button>("HBoxContainer/Button2").ButtonDown += () => GetTree().ChangeSceneToFile("res://Stages/TestStage2.tscn");
+        
+    }
+}
