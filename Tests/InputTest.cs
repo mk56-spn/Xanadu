e9526bba6f4fa8649bf49af4a4a3b45e76393b88
mk56-spn <darioheadleysilva@gmail.com>
2023-06-27 11:46:38 +0200

    Add a scene for testing inputs ( and potentially more things in the future )

---
+++ Tests/InputTest.cs
@@ -0,0 +1,57 @@
+using System.Collections.Generic;
+using Godot;
+
+namespace XanaduProject.Tests;
+
+[GlobalClass]
+public partial class InputTest : CenterContainer
+{
+    public InputTest()
+    {
+        var container = new HBoxContainer();
+        AddChild(container);
+
+        foreach (var child in _addKeys)
+            container.AddChild(child);
+    }
+
+    private readonly List<InputRect> _addKeys = new()
+    {
+        new InputRect
+        {
+            Position = new Vector2(400,400),
+            Key = "main",
+            ColourOn = Colors.Yellow
+        },
+        new InputRect
+        {
+            Position = new Vector2(400,400),
+            Key = "R1",
+            ColourOn = Colors.Green
+        },
+        new InputRect
+        {
+            Position = new Vector2(400,400),
+            Key = "R2",
+            ColourOn = Colors.Orange
+        }
+    };
+
+    private ColorRect InputBox() =>
+        new() { Size = new Vector2(100, 100) };
+
+    private partial class InputRect : ColorRect
+    {
+        public Color ColourOn { get; set; }
+        public string Key { get; set; }
+
+        public InputRect() =>
+           CustomMinimumSize = new Vector2(100, 100);
+        
+        public override void _Process(double delta)
+        {
+            base._Process(delta);
+            Color = Input.IsActionPressed(Key) ? ColourOn : Colors.Black;
+        }
+    }
+}
