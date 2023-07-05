6bf8dbfa1c489b4dee9c4c2fe0774e4598d4f850
mk56-spn <darioheadleysilva@gmail.com>
2023-07-05 19:04:39 +0200

    Remove "TestTrackInfo.cs" in favour of properly loading resource instance through folder system.

--- DataStructure/TrackInfo.cs
+++ DataStructure/TrackInfo.cs
@@ -2,14 +2,17 @@ using Godot;
 
 namespace XanaduProject.DataStructure;
 
+[GlobalClass]
 public partial class TrackInfo : Resource
 {
+    [Export]
     public double Offset { get; set; }
     // Currently bpm changes are not supported.
+    [Export]
     public double Bpm { get; set; }
-
+    [Export]
     public int Measures { get; set; }
-
+    [Export(PropertyHint.ResourceType)]
     public AudioStream Track { get; set; } = null!;
 }
     
