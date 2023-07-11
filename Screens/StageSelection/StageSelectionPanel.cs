fd562bb0989d0d32e3580bb637908a81508bae63
mk56-spn <darioheadleysilva@gmail.com>
2023-07-11 13:28:48 +0200

    Update "AudioSource.cs" singleton information upon requesting entry to a stage.

--- Screens/StageSelection/StageSelectionPanel.cs
+++ Screens/StageSelection/StageSelectionPanel.cs
@@ -1,12 +1,20 @@
 using Godot;
 using XanaduProject.DataStructure;
+using XanaduProject.Singletons;
 
 namespace XanaduProject.Screens.StageSelection;
 
 public partial class StageSelectionPanel : PanelContainer
 {
-    public StageSelectionPanel(StageInfo stageInfo)
+    private readonly StageInfo stageInfo;
+
+    public StageSelectionPanel(StageInfo stageInfo) =>
+        this.stageInfo = stageInfo;
+
+    public override void _Ready()
     {
+        base._Ready();
+
         Label label = new Label { Text = stageInfo.Title };
         Button selectButton = new Button { Text = "PLAY" };
         VBoxContainer container = new VBoxContainer { CustomMinimumSize = new Vector2(150, 0) };
@@ -15,6 +23,10 @@ public partial class StageSelectionPanel : PanelContainer
         container.AddChild(label);
         container.AddChild(selectButton);
 
-        selectButton.Pressed += () => GetTree().ChangeSceneToPacked(stageInfo.Stage);
+        selectButton.Pressed += () =>
+        {
+            GetNode<AudioSource>("/root/GlobalAudio").SetTrack(stageInfo.TrackInfo);
+            GetTree().ChangeSceneToPacked(stageInfo.Stage);
+        };
     }
 }
