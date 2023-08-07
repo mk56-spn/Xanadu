cd41c95bc64ae797e3f88fe01768455d0599020f
mk56-spn <darioheadleysilva@gmail.com>
2023-08-07 19:10:46 +0200

    Extract Carousel into its own class

--- Screens/StageSelection/StageSelection.cs
+++ Screens/StageSelection/StageSelection.cs
@@ -1,8 +1,6 @@
 // Copyright (c) mk56_spn <dhsjplt@gmail.com>. Licensed under the GNU General Public Licence (2.0).
 // See the LICENCE file in the repository root for full licence text.
 
-using System;
-using System.Linq;
 using Godot;
 using XanaduProject.DataStructure;
 using XanaduProject.Screens.Player;
@@ -12,78 +10,26 @@ namespace XanaduProject.Screens.StageSelection
 {
     public partial class StageSelection : Control
     {
-        private const double transition = 0.5;
-
-        [Export]
-        private HBoxContainer trackList = null!;
-
         [Export] private Button startButton = null!;
-        [Export] private ScrollContainer scrollContainer = null!;
 
-        private StageInfo activeInfo = null!;
-
-        private Tween? opacityTween;
+        public StageInfo ActiveInfo = null!;
 
         public override void _Ready()
         {
             base._Ready();
 
-            trackList.AddThemeConstantOverride("separation", 100);
-
-            var dir = DirAccess.Open("res://Resources/Stages/");
-
-            foreach (string? folder in dir.GetDirectories())
-            {
-                // Retrieves information for the subsequent loading of it's stage.
-                StageInfo stageInfo = ResourceLoader.Load<StageInfo>($"res://Resources/Stages/{folder}/information.tres");
-
-                trackList.AddChild(new StageSelectionPanel(stageInfo));
-            }
-
-            Tween? scrollTween = null;
-
-            // TODO : this should all be moved to its own class along with all the stage selection carousel stuff honestly.
-            foreach (var panel in trackList.GetChildren().OfType<StageSelectionPanel>())
-                panel.FocusEntered += () =>
-                {
-                    scrollTween = CreateTween();
-                    scrollTween.TweenProperty(scrollContainer, "scroll_horizontal", panel.Position.X + panel.Size.X / 2,
-                            transition)
-                        .SetTrans(Tween.TransitionType.Sine)
-                        .SetEase(Tween.EaseType.Out);
-
-                    activeInfo = panel.StageInfo;
-
-                    updateOpacity(panel.GetIndex());
-                };
-
-            trackList.GetChild<StageSelectionPanel>(0).GrabFocus();
+            AddChild(new StageSelectionCarousel(this));
 
             startButton.Pressed += () =>
             {
-                GetNode<AudioSource>("/root/GlobalAudio").SetTrack(activeInfo.TrackInfo);
+                GetNode<AudioSource>("/root/GlobalAudio").SetTrack(ActiveInfo.TrackInfo);
                 loadStage();
             };
         }
 
-        // TODO : this should all be moved to its own class along with all the stage selection carousel stuff honestly.
-        private void updateOpacity(int focusedIndex)
-        {
-            foreach (var panel in trackList.GetChildren().OfType<StageSelectionPanel>())
-            {
-                var alpha = panel.Modulate;
-                alpha.A = 1f / (1 + Math.Abs(panel.GetIndex() - focusedIndex));
-
-                opacityTween = CreateTween();
-                opacityTween.TweenProperty(panel, "modulate", alpha, transition)
-                    .SetTrans(Tween.TransitionType.Sine)
-                    .SetEase(Tween.EaseType.Out);
-            }
-        }
-
         private void loadStage()
         {
-            PlayerLoader player = new PlayerLoader(activeInfo);
+            PlayerLoader player = new PlayerLoader(ActiveInfo);
 
             GetTree().Root.AddChild(player);
             GetTree().CurrentScene = player;
