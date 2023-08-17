3d9871484f554b5925766d81b169e4757a1809ed
mk56-spn <darioheadleysilva@gmail.com>
2023-08-17 13:58:09 +0200

    Implement Transition.cs and corresponding scene

--- Screens/Player/PlayerLoader.cs
+++ Screens/Player/PlayerLoader.cs
@@ -1,15 +1,25 @@
 // Copyright (c) mk56_spn <dhsjplt@gmail.com>. Licensed under the GNU General Public Licence (2.0).
 // See the LICENCE file in the repository root for full licence text.
 
+using Chickensoft.AutoInject;
 using Godot;
+using SuperNodes.Types;
 using XanaduProject.DataStructure;
+using XanaduProject.Screens.StageUI;
 using XanaduProject.Singletons;
 
 namespace XanaduProject.Screens.Player
 {
-    public partial class PlayerLoader : Node
+    [SuperNode(typeof(Provider))]
+    public partial class PlayerLoader : Node, IProvide<StageInfo>
     {
+        public override partial void _Notification(int what);
+
+
         private readonly StageInfo stageInfo;
+
+        // Cache StageInfo at a loader level for downstream consumption
+        StageInfo IProvide<StageInfo>.Value() => stageInfo;
 
         public PlayerLoader (StageInfo stageInfo)
         {
@@ -24,9 +34,20 @@ namespace XanaduProject.Screens.Player
 
             audioSource.SetTrack(stageInfo.TrackInfo);
 
-            createPlayer(audioSource);
+            PackedScene transitionScene = ResourceLoader.Load<PackedScene>("res://Screens/StageUI/Transition.tscn");
+            CanvasLayer layer = new CanvasLayer();
+
+            Transition transition = transitionScene.Instantiate<Transition>();
+
+            AddChild(layer);
+            layer.AddChild(transition);
 
             TreeExited += () => audioSource.Stream = null;
+
+            // Delay player creation until transition is over.
+            transition.TransitionFinished += (_, _) => createPlayer(audioSource);
+
+            Provide();
         }
 
         private void createPlayer(AudioSource audioSource)
---
+++ Screens/StageUI/Transition.cs
@@ -0,0 +1,43 @@
+// Copyright (c) mk56_spn <dhsjplt@gmail.com>. Licensed under the GNU General Public Licence (2.0).
+// See the LICENCE file in the repository root for full licence text.
+
+using System;
+using Chickensoft.AutoInject;
+using Godot;
+using SuperNodes.Types;
+using XanaduProject.DataStructure;
+
+namespace XanaduProject.Screens.StageUI
+{
+    [SuperNode(typeof(Dependent))]
+    public partial class Transition : Control
+    {
+        public override partial void _Notification(int what);
+
+        [Export]
+        private Label stageText { get; set; } = null!;
+        [Export]
+        private Label variantText { get; set; } = null!;
+        [Export]
+        private Label genreText { get; set; } = null!;
+        [Export]
+        private AnimationPlayer animation { get; set; } = null!;
+
+        [Dependency] private StageInfo stageInfo  => DependOn<StageInfo>();
+
+        /// <summary>
+        /// Emitted after the transition animation is finished
+        /// </summary>
+        public event EventHandler? TransitionFinished;
+
+        public void OnResolved()
+        {
+            stageText.Text = stageInfo.Title;
+            animation.AnimationFinished += _ =>
+            {
+                Hide();
+                TransitionFinished?.Invoke(this, EventArgs.Empty);
+            };
+        }
+    }
+}
