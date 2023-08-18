b934d84a01de16a1b8049268affaf3b9ea837b31
mk56-spn <darioheadleysilva@gmail.com>
2023-08-18 20:56:26 +0200

    Add access to notes in a stage.

--- Screens/Stage.cs
+++ Screens/Stage.cs
@@ -1,7 +1,10 @@
 // Copyright (c) mk56_spn <dhsjplt@gmail.com>. Licensed under the GNU General Public Licence (2.0).
 // See the LICENCE file in the repository root for full licence text.
 
+using System.Collections.Generic;
+using System.Linq;
 using Godot;
+using XanaduProject.Composer;
 using XanaduProject.Perceptions;
 
 namespace XanaduProject.Screens
@@ -10,11 +13,18 @@ namespace XanaduProject.Screens
     public partial class Stage : WorldEnvironment
     {
         public readonly Core Core;
+        public IEnumerable<Note> Notes { get; private set; } = null!;
 
         public Stage()
         {
             var coreScene = ResourceLoader.Load<PackedScene>("res://Perceptions/Core.tscn");
             AddChild(Core = coreScene.Instantiate<Core>());
+        }
+
+        public override void _Ready()
+        {
+            base._Ready();
+            Notes = GetChildren().OfType<Note>();
         }
     }
 }
