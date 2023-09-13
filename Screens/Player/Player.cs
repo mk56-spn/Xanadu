f96f25df945c3e49678cc02688a37017faf0d2f8
mk56-spn <darioheadleysilva@gmail.com>
2023-09-14 00:10:37 +0200

    Extract player.cs duplicate info into StageHandler.cs

--- Screens/Player/Player.cs
+++ Screens/Player/Player.cs
@@ -4,26 +4,17 @@
 using Chickensoft.AutoInject;
 using Godot;
 using SuperNodes.Types;
-using XanaduProject.Audio;
-using XanaduProject.DataStructure;
 using XanaduProject.Screens.StageUI;
 
 namespace XanaduProject.Screens.Player
 {
     [SuperNode(typeof(Provider))]
-    public partial class Player : Control, IProvide<TrackHandler>, IProvide<ScoreProcessor>
+    public partial class Player : StageHandler, IProvide<ScoreProcessor>
     {
         public override partial void _Notification(int what);
 
-        public TrackHandler Value() => trackHandler;
         ScoreProcessor IProvide<ScoreProcessor>.Value() => scoreProcessor;
-
         private ScoreProcessor scoreProcessor = null!;
-        private TrackHandler trackHandler = new TrackHandler();
-        private readonly StageInfo stageInfo;
-        private readonly Stage stage;
-
-        private Camera2D camera = new Camera2D();
 
         private readonly StagePause pauseMenu = ResourceLoader
             .Load<PackedScene>("res://Screens/StageUI/StagePause.tscn").Instantiate<StagePause>();
@@ -31,33 +22,21 @@ namespace XanaduProject.Screens.Player
         private readonly ComboCounter comboCounter  = ResourceLoader
             .Load<PackedScene>("res://Screens/StageUI/ComboCounter.tscn").Instantiate<ComboCounter>();
 
-        public Player (StageInfo stageInfo)
+        public Player() : base(new Camera2D())
         {
-            this.stageInfo = stageInfo;
-            ProcessMode = ProcessModeEnum.Always;
-
-            stage = stageInfo.GetStage();
-            AddChild(stage);
         }
 
         public override void _Ready()
         {
             base._Ready();
-            trackHandler.SetTrack(stageInfo.TrackInfo);
-            GetTree().Paused = true;
-
-            AddChild(scoreProcessor = new ScoreProcessor(stage));
-            AddChild(trackHandler);
 
             Provide();
 
-            AddChild(camera);
-            loadUi();
-            trackHandler.StartTrack();
+            AddChild(scoreProcessor = new ScoreProcessor(Stage));
 
-            // We do not want the stage to process whilst the preempt is still underway.
-            trackHandler.OnPreemptComplete += (_, _) => GetTree().Paused = false;
-            stage.Core.OnDeath += () => pauseMenu.Show();
+            loadUi();
+
+            Stage.Core.OnDeath += () => pauseMenu.Show();
             pauseMenu.RestartRequest += () => GetParent<PlayerLoader>().LoadPlayer();
         }
 
@@ -65,7 +44,7 @@ namespace XanaduProject.Screens.Player
         {
             CanvasLayer canvasLayer = new CanvasLayer();
 
-            camera.AddChild(canvasLayer);
+            Camera.AddChild(canvasLayer);
             canvasLayer.AddChild(pauseMenu);
             canvasLayer.AddChild(comboCounter);
         }
@@ -73,7 +52,7 @@ namespace XanaduProject.Screens.Player
         public override void _PhysicsProcess(double delta)
         {
             base._PhysicsProcess(delta);
-            camera.Position = stage.Core.Position;
+            Camera.Position = Stage.Core.Position;
         }
     }
 }
--- Screens/StageHandler.cs
+++ Screens/StageHandler.cs
@@ -13,13 +13,9 @@ namespace XanaduProject.Screens
     /// Base class for any classes that are tasked with managing a stage.
     /// </summary>
     [SuperNode(typeof(Provider))]
-    public partial class StageHandler : CanvasLayer, IProvide<TrackHandler>, IProvide<Stage>
+    public abstract partial class StageHandler : Control, IProvide<TrackHandler>, IProvide<Stage>
     {
         public override partial void _Notification(int what);
-
-        protected Stage Stage = null!;
-
-        private StageInfo stageInfo = null!;
 
         /// <summary>
         /// A resource containing information about a stage that may be useful outside of gameplay.
@@ -34,20 +30,30 @@ namespace XanaduProject.Screens
             }
         }
 
-        private readonly TrackHandler trackHandler = new TrackHandler();
+        protected Camera2D Camera;
+        protected Stage Stage = null!;
+        private StageInfo stageInfo = null!;
 
+        private readonly TrackHandler trackHandler = new TrackHandler();
         TrackHandler IProvide<TrackHandler>.Value() => trackHandler;
         public Stage Value() => Stage;
+
+        protected StageHandler (Camera2D camera)
+        {
+            Camera = camera;
+            ProcessMode = ProcessModeEnum.Always;
+
+            AddChild(trackHandler);
+            AddChild(camera);
+        }
 
         public override void _Ready()
         {
             base._Ready();
 
             GetTree().Paused = true;
-            ProcessMode = ProcessModeEnum.Always;
 
             AddChild(Stage);
-            AddChild(trackHandler);
 
             trackHandler.SetTrack(StageInfo.TrackInfo);
             trackHandler.StartTrack();
