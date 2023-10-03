614dcac2e2a90364b40bca77948965b7b24ad6e2
mk56-spn <darioheadleysilva@gmail.com>
2023-10-03 19:56:36 +0200

    remove obsolete code from PropertyContainer.cs

--- Composer/ComposerUI/PropertyContainer.cs
+++ Composer/ComposerUI/PropertyContainer.cs
@@ -1,65 +1,17 @@
 // Copyright (c) mk56_spn <dhsjplt@gmail.com>. Licensed under the GNU General Public Licence (2.0).
 // See the LICENCE file in the repository root for full licence text.
 
-using System.Collections.Generic;
-using System.Linq;
-using Chickensoft.AutoInject;
 using Godot;
-using SuperNodes.Types;
+using XanaduProject.Composer.ComposerUI.PropertyGroups;
 
 namespace XanaduProject.Composer.ComposerUI
 {
-    [SuperNode(typeof(Dependent))]
     public partial class PropertyContainer : VBoxContainer
     {
-        public override partial void _Notification(int what);
-
-        [Dependency]
-        private List<Node> selected => DependOn<List<Node>>();
-
-        private SpinBox layerBox;
-        private ColorPicker colourPicker;
-
         public PropertyContainer ()
         {
-            layerBox = new SpinBox { Step = 1, MinValue = -100 };
-            colourPicker = new ColorPicker
-            {
-                ColorMode = ColorPicker.ColorModeType.Hsv,
-                SlidersVisible = false,
-                ColorModesVisible = false,
-                PresetsVisible = false,
-
-                // TODO : Broken rn. needs fix
-                SamplerVisible = false,
-            };
-
-            setupProperties();
-        }
-
-        private void setupProperties()
-        {
-            AddChild(layerBox);
-            AddChild(colourPicker);
-
-            layerBox.ValueChanged += val =>
-            {
-                foreach (var selectableNode in selected.OfType<Node2D>())
-                    selectableNode.ZIndex = (int)val;
-            };
-
-            colourPicker.ColorChanged += val =>
-            {
-                foreach (var selectableNode in selected.OfType<Node2D>())
-                    selectableNode.SelfModulate = val;
-            };
-        }
-
-        public override void _Process(double delta)
-        {
-            base._Process(delta);
-
-            Visible = selected.Any();
+            AddChild(new GlobalPropertyGroup());
+            AddChild(new LinePropertyGroup());
         }
     }
 }
