35dbb23690be2ba1b46cc85d6fee07f8ebb5dd58
mk56-spn <darioheadleysilva@gmail.com>
2023-10-03 21:07:20 +0200

    Add line cap ending selection

--- Composer/ComposerUI/PropertyGroups/LinePropertyGroup.cs
+++ Composer/ComposerUI/PropertyGroups/LinePropertyGroup.cs
@@ -1,6 +1,7 @@
 // Copyright (c) mk56_spn <dhsjplt@gmail.com>. Licensed under the GNU General Public Licence (2.0).
 // See the LICENCE file in the repository root for full licence text.
 
+using System;
 using System.Linq;
 using Godot;
 
@@ -13,15 +14,36 @@ namespace XanaduProject.Composer.ComposerUI.PropertyGroups
         protected override Color GroupColour => Colors.Yellow;
 
         private Slider width = new HSlider { MinValue = 0, MaxValue = 20 };
+        private OptionButton lineCapPicker = new OptionButton();
 
         public LinePropertyGroup ()
         {
             AddChild(width);
+            AddChild(lineCapPicker);
+
+            lineCapPicker.AddItem("None");
+            lineCapPicker.AddItem("Flat");
+            lineCapPicker.AddItem("Rounded");
 
             width.ValueChanged += val =>
             {
                 foreach (var selectableNode in Selected.OfType<Line2D>())
                     selectableNode.Width = (float)val;
+            };
+
+
+            lineCapPicker.ItemSelected += index =>
+            {
+                Line2D.LineCapMode result = index switch
+                {
+                    0 => Line2D.LineCapMode.None,
+                    1 => Line2D.LineCapMode.Box,
+                    2 => Line2D.LineCapMode.Round,
+                    _ => throw new ArgumentOutOfRangeException(nameof(index), index, null)
+                };
+
+                foreach (var selectableNode in Selected.OfType<Line2D>())
+                    selectableNode.EndCapMode = selectableNode.BeginCapMode = result;
             };
         }
 
