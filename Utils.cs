a1fa5bf2f5d8999d6174e9146ef7135641894ad1
mk56-spn <darioheadleysilva@gmail.com>
2023-10-06 22:19:36 +0200

    Add utils class for general purpose methods

---
+++ Utils.cs
@@ -0,0 +1,37 @@
+// Copyright (c) mk56_spn <dhsjplt@gmail.com>. Licensed under the GNU General Public Licence (2.0).
+// See the LICENCE file in the repository root for full licence text.
+
+
+using Godot;
+
+namespace XanaduProject
+{
+    public static class Utils
+    {
+        public static Rect2 GetNonNegativeRect(Vector2 start, Vector2 end, bool centered = false)
+        {
+            Vector2 rectSize = new Vector2(Mathf.Abs(end.X - start.X), Mathf.Abs(end.Y - start.Y));
+
+            // After the transform we check to see if the position of the rectangle needs changing.
+            Vector2 rectanglePosition = new Vector2(Mathf.Min(start.X, end.X), Mathf.Min(start.Y, end.Y));
+
+            return new Rect2 { Size = rectSize, Position = centered ? rectanglePosition + rectSize / 2 : rectanglePosition };
+        }
+
+        public static Rect2 PointBoundingBox(Vector2[] points)
+        {
+            Vector2 rectStart = Vector2.Inf;
+            Vector2 rectEnd = -Vector2.Inf;
+
+            foreach (Vector2 point in points)
+            {
+                rectStart.X = Mathf.Min(rectStart.X, point.X);
+                rectStart.Y = Mathf.Min(rectStart.Y, point.Y);
+                rectEnd.X = Mathf.Max(rectEnd.X, point.X);
+                rectEnd.Y = Mathf.Max(rectEnd.Y, point.Y);
+            }
+
+            return new Rect2(rectStart, rectEnd - rectStart);
+        }
+    }
+}
