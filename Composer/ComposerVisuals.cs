69805c5dac4e7b8331e390f3ac346e3e373adefc
mk56-spn <darioheadleysilva@gmail.com>
2024-01-09 19:51:26 +0100

    Move composer selection visuals to their own class

---
+++ Composer/ComposerVisuals.cs
@@ -0,0 +1,38 @@
+// Copyright (c) mk56_spn <dhsjplt@gmail.com>. Licensed under the GNU General Public Licence (2.0).
+// See the LICENCE file in the repository root for full licence text.
+
+using Godot;
+using XanaduProject.Serialization.Elements;
+
+namespace XanaduProject.Composer
+{
+    public partial class ComposerVisuals(ComposerRenderMaster composer) : Control
+    {
+        private Label infoLabel = new Label();
+
+        public override void _Ready() =>  AddChild(infoLabel);
+        public override void _PhysicsProcess(double delta)
+        {
+            infoLabel.Text = "Canvas transform: " +  GetViewport().CanvasTransform.Origin;
+            QueueRedraw();
+        }
+
+        public override void _Draw()
+        {
+            Vector2 center = Vector2.Zero;
+
+            foreach (var rid in composer.SelectedAreas)
+            {
+                Element element = composer.Dictionary[rid.Item1].Element;
+                center += element.Position;
+
+                DrawSetTransformMatrix(element.Transform().Translated(GetViewport().CanvasTransform.Origin));
+
+                DrawRect(new Rect2(-element.Size() / 2, element.Size()), Colors.DeepPink with { A = 0.3f });
+                DrawRect(new Rect2(-element.Size() / 2, element.Size()), Colors.DeepPink, false, -0.1f);
+            }
+
+            DrawSetTransformMatrix(Transform2D.Identity);
+        }
+    }
+}
