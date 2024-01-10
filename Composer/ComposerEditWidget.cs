64a7de1a3f39679471262abc64a66ca7277e55ef
mk56-spn <darioheadleysilva@gmail.com>
2024-01-10 16:30:03 +0100

    Update widget with skew property

--- Composer/ComposerEditWidget.cs
+++ Composer/ComposerEditWidget.cs
@@ -2,6 +2,7 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using Godot;
+using XanaduProject.Serialization.Elements;
 
 namespace XanaduProject.Composer
 {
@@ -9,6 +10,7 @@ namespace XanaduProject.Composer
     {
         [Export] private Slider scaleY = null!;
         [Export] private Slider scaleX = null!;
+        [Export] private Slider skew  = null!;
         [Export] private ColorPicker picker = null!;
 
         private ComposerRenderMaster composer = null!;
@@ -24,17 +26,17 @@ namespace XanaduProject.Composer
 
                 if (target == null) return;
 
-                Vector2 vector2 = composer.GetElementForArea(target.Value).Scale;
+                Element element = composer.GetElementForArea(target.Value);
 
-                scaleX.SetValueNoSignal(vector2.X);
-                scaleY.SetValueNoSignal(vector2.Y);
+                scaleX.SetValueNoSignal(element.Scale.X);
+                scaleY.SetValueNoSignal(element.Scale.Y);
+                skew.SetValueNoSignal(element.Skew);
 
                 picker.Color = composer.GetElementForArea(target.Value).Colour;
             }
         }
 
         private Vector2 scale => new Vector2((float)scaleX.Value, (float)scaleY.Value);
-
 
         public static ComposerEditWidget Create(ComposerRenderMaster composer)
         {
@@ -46,8 +48,14 @@ namespace XanaduProject.Composer
 
         public override void _EnterTree()
         {
+            AddChild(new RotationWidget(composer, this));
             scaleX.ValueChanged += _ => setScale();
             scaleY.ValueChanged += _ => setScale();
+            skew.ValueChanged += value =>
+            {
+                composer.SkewElement(target!.Value, (float)value);
+                composer.QueueRedraw();
+            };
             picker.ColorChanged += color => composer.TintElement(target!.Value, color);
         }
 
