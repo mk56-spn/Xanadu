c12a64cb32eb7c0e0c262c4c5940cd82e617593b
mk56_spn <dhsjplt@gmail.com>
2024-11-24 03:20:55 +0100

    Align mouse movement with screen regardless of zoom

--- Composer/PanningCamera.cs
+++ Composer/PanningCamera.cs
@@ -5,40 +5,40 @@ using Godot;
 
 namespace XanaduProject.Composer
 {
-    public partial class PanningCamera : Camera2D
-    {
-        // Currently we zoom towards the screen center which is not ideal.
-        private static readonly Vector2 min_zoom = new Vector2(0.5f, 0.5f);
-        private static readonly Vector2 max_zoom = new Vector2(4f, 4f);
+	public partial class PanningCamera : Camera2D
+	{
+		// Currently we zoom towards the screen center which is not ideal.
+		private static readonly Vector2 min_zoom = new(0.5f, 0.5f);
+		private static readonly Vector2 max_zoom = new(4f, 4f);
 
-        public override void _Ready()
-        {
-            base._Ready();
+		public override void _Ready() =>
+			PositionSmoothingEnabled = true;
 
-            PositionSmoothingEnabled = true;
-        }
+		private Vector2 mousepos;
 
-        public override void _Input(InputEvent @event)
-        {
-            base._Input(@event);
+		public override void _Input(InputEvent @event)
+		{
+			base._Input(@event);
 
-            if (@event is InputEventMouseButton button)
-                switch (button.ButtonIndex)
-                {
-                    case MouseButton.WheelDown:
-                        Zoom = (Zoom -= Zoom / 30).Clamp(min_zoom, max_zoom);
-                        break;
-                    case MouseButton.WheelUp:
-                        Zoom = (Zoom += Zoom / 30).Clamp(min_zoom, max_zoom);
-                        break;
-                    default:
-                        return;
-                }
+			if (@event is InputEventMouseButton button)
+				switch (button.ButtonIndex)
+				{
+					case MouseButton.WheelDown:
+						Zoom = (Zoom -= Zoom / 30).Clamp(min_zoom, max_zoom);
+						break;
+					case MouseButton.WheelUp:
+						Zoom = (Zoom += Zoom / 30).Clamp(min_zoom, max_zoom);
+						break;
+					default:
+						return;
+				}
 
-            if (@event is not InputEventMouseMotion { ButtonMask: MouseButtonMask.Middle } mouse) return;
-            Offset -= mouse.Relative;
+			mousepos = GetGlobalMousePosition();
 
-            GetViewport().SetInputAsHandled();
-        }
-    }
+			if (@event is not InputEventMouseMotion { ButtonMask: MouseButtonMask.Middle } mouse) return;
+			Offset -= mouse.ScreenRelative / Zoom;
+
+			GetViewport().SetInputAsHandled();
+		}
+	}
 }
