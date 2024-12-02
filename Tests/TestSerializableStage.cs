6635d7646b841f4889f20e805d165a1b711770d4
mk56_spn <dhsjplt@gmail.com>
2024-12-02 21:16:35 +0100

    Update tests and associated scenes

--- Tests/TestSerializableStage.cs
+++ Tests/TestSerializableStage.cs
@@ -2,6 +2,7 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using System.Collections.Generic;
+using System.Linq;
 using Godot;
 using XanaduProject.Serialization.Elements;
 using XanaduProject.Serialization.SerialisedObjects;
@@ -16,14 +17,14 @@ namespace XanaduProject.Tests
     {
         public TestSerializableStage (int noteCount, int textureElementCount, int textElementCount)
         {
-            addTextures(textElementCount, textureElementCount, noteCount);
+            addTextures(textElementCount, textureElementCount);
         }
         public TestSerializableStage ()
         {
-            addTextures(RandRange(0, 5000), RandRange(0, 10000), RandRange(0, 500));
+            addTextures(RandRange(0, 5000), RandRange(0, 10000));
         }
 
-        private void addTextures(int textElementCount, int textureElementCount, int noteCount)
+        private void addTextures(int textElementCount, int textureElementCount)
         {
             List<Element> tempElements = new List<Element>();
             DynamicTextures = new Texture[10];
@@ -35,27 +36,26 @@ namespace XanaduProject.Tests
                 {
                     Colour = new Color(Randf(), Randf(), Randf()),
                     Text = "Testing",
-                    TextSize = RandRange(5, 300),
-                    Scale = new Vector2((float)RandRange(0.1, 3), (float)RandRange(0.1, 3)),
-                    Skew = RandRange(0, 1),
+                    TextSize = 20,
+                    Scale = Vector2.One,
+                    Skew = 0,
                     Position = new Vector2(Randf(), Randf()) * 100000,
                     Group = RandRange(0, 900),
-                    Rotation = RandRange(0, 1080),
+                    Rotation = 0,
                     Zindex = RandRange(1, 1000)
                 });
             }
 
-            for (int j = 0; j < noteCount; j++)
-                tempElements.Add(new NoteElement
-                {
-                    TimingPoint = RandRange(1, 800) * (60 / 200f),
-                    Colour = Colors.White,
-                    Scale = Vector2.One,
-                    Position = new Vector2(Randf(), Randf()) * 1000,
-                    Group = RandRange(0, 900),
-                    Rotation = RandRange(0, 1080),
-                    Zindex = RandRange(1, 1000)
-                });
+            tempElements.AddRange(notes.Select(t => new NoteElement
+            {
+                Colour = Colors.White,
+                TimingPoint = t,
+                Position = new Vector2(Randf(), -Randf()) * 1000,
+                Scale = Vector2.One,
+                Group = RandRange(0, 900),
+                Rotation = RandRange(0, 1080),
+                Zindex = RandRange(1, 1000)
+            }));
 
             for (int j = 0; j < textureElementCount; j++)
             {
@@ -64,22 +64,20 @@ namespace XanaduProject.Tests
                     Colour = new Color(Randf(), Randf(), Randf()),
                     Scale = new Vector2((float)RandRange(0.1, 3), (float)RandRange(0.1, 3)),
                     Skew = RandRange(0, 1),
-                    Position = new Vector2(Randf(), Randf()) * 100000,
+                    Position = new Vector2(Randf(), -Randf()) * 100000,
                     Group = RandRange(0, 900),
                     Rotation = RandRange(0, 1080),
                     Zindex = RandRange(1, 1000)
                 });
             }
 
-            float separation = 50;
-            for (int i = 0; i < 1000; i++)
+            for (int i = 0; i < 100; i++)
             {
                 tempElements.Add(new PhysicsElement
                 {
-                    Extents = new Vector2(separation, separation),
                     Colour = Colors.White,
                     Scale = Vector2.One,
-                    Position = new Vector2(50 * i, 50),
+                    Position = new Vector2(200 + 32 * i, float.Sin(i) * 16 -8 * i),
                     Group = RandRange(0, 900),
                     Zindex = RandRange(1, 1000)
                 });
@@ -90,10 +88,14 @@ namespace XanaduProject.Tests
                 if (i % 2 == 0)
                     DynamicTextures[i] = new GradientTexture2D
                     {
-                        FillFrom = new Vector2(Randf(), Randf()),
+                        FillFrom = new Vector2(0.5f, 0.5f),
                         Fill = (GradientTexture2D.FillEnum)RandRange(0, 2),
-                        FillTo = new Vector2(Randf(), Randf()),
-                        Gradient = new Gradient { Colors = [Colors.Azure, Colors.Bisque, Colors.White with { A = 0.4f } ] , Offsets = [ 0, 0.6f, 0.9f ] }
+                        FillTo = new Vector2(1.0f, 0.5f),
+                        Gradient = new Gradient
+                        {
+                            Colors = [Colors.Transparent, Colors.White, Colors.Transparent] ,
+                            Offsets = [ 0.85f, 0.95f, 1.0f ]
+                        }
                     };
 
                 else
@@ -112,5 +114,16 @@ namespace XanaduProject.Tests
 
             Elements = tempElements.ToArray();
         }
+
+        private readonly float[] notes =
+        [
+            0.6f,
+            1.2f,
+            1.8f,
+            2.4f,
+            6f,
+            9.3f,
+            12f
+       ];
     }
 }
