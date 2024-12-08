c886cdde0ab58bd8c8ec26c204a2b945411bc870
mk56_spn <darioheadleysilva@gmail.com>
2024-12-08 12:02:44 +0100

    Update InputTest.cs indentation

--- Tests/InputTest.cs
+++ Tests/InputTest.cs
@@ -6,72 +6,72 @@ using Godot;
 
 namespace XanaduProject.Tests
 {
-    [GlobalClass]
-    public partial class InputTest : CenterContainer
-    {
-        private readonly List<InputRect> addKeys = new List<InputRect>
-        {
-            new InputRect
-            {
-                Position = new Vector2(400, 400),
-                Key = "main",
-                ColourOn = Colors.Yellow
-            },
-            new InputRect
-            {
-                Position = new Vector2(400, 400),
-                Key = "R1",
-                ColourOn = Colors.Green
-            },
-            new InputRect
-            {
-                Position = new Vector2(400, 400),
-                Key = "R2",
-                ColourOn = Colors.Orange
-            }
-        };
+	[GlobalClass]
+	public partial class InputTest : CenterContainer
+	{
+		private readonly List<InputRect> addKeys = new List<InputRect>
+		{
+			new InputRect
+			{
+				Position = new Vector2(400, 400),
+				Key = "main",
+				ColourOn = Colors.Yellow
+			},
+			new InputRect
+			{
+				Position = new Vector2(400, 400),
+				Key = "R1",
+				ColourOn = Colors.Green
+			},
+			new InputRect
+			{
+				Position = new Vector2(400, 400),
+				Key = "R2",
+				ColourOn = Colors.Orange
+			}
+		};
 
-        public InputTest()
-        {
-            var container = new HBoxContainer();
-            AddChild(container);
+		public InputTest()
+		{
+			var container = new HBoxContainer();
+			AddChild(container);
 
-            foreach (var child in addKeys)
-                container.AddChild(child);
-        }
+			foreach (var child in addKeys)
+				container.AddChild(child);
+		}
 
-        private ColorRect inputBox()
-        {
-            return new ColorRect { Size = new Vector2(100, 100) };
-        }
+		private ColorRect inputBox()
+		{
+			return new ColorRect { Size = new Vector2(100, 100) };
+		}
 
-        private partial class InputRect : ColorRect
-        {
-            public InputRect()
-            {
-                CustomMinimumSize = new Vector2(100, 100);
-            }
+		private partial class InputRect : ColorRect
+		{
+			public InputRect()
+			{
+				CustomMinimumSize = new Vector2(100, 100);
+			}
 
-            public Color ColourOn { get; set; }
-            public string? Key { get; set; }
+			public Color ColourOn { get; set; }
+			public string? Key { get; set; }
 
-            public override void _Ready()
-            {
-                base._Ready();
+			public override void _Ready()
+			{
+				base._Ready();
 
-                AddChild(new Label
-                {
-                    Text = Key,
-                    LayoutMode = 1,
-                    AnchorsPreset = 8
-                });
-            }
+				AddChild(new Label
+				{
+					Text = Key,
+					LayoutMode = 1,
+					AnchorsPreset = 8
+				});
+			}
 
-            public override void _Process(double delta)
-            {
-                base._Process(delta);
-                Color = Input.IsActionPressed(Key) ? ColourOn : Colors.Black;
-            }
-        }
-    }
+			public override void _Process(double delta)
+			{
+				base._Process(delta);
+				Color = Input.IsActionPressed(Key) ? ColourOn : Colors.Black;
+			}
+		}
+	}
 }
