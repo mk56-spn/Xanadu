418ee2ec06f7f2cbe5e41c0f21002887e8aaf482
mk56_spn <darioheadleysilva@gmail.com>
2024-12-25 17:03:19 +0100

    Rework judgement system slightly

--- DataStructure/JudgementInfo.cs
+++ DataStructure/JudgementInfo.cs
@@ -2,6 +2,7 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using System;
+using Godot;
 
 namespace XanaduProject.DataStructure
 {
@@ -13,6 +14,7 @@ namespace XanaduProject.DataStructure
         public static Judgement GetJudgement(double deviation) =>
             deviation switch
             {
+                < 8.3 => Judgement.FlawlessP,
                 < 16 => Judgement.Flawless,
                 < 27 => Judgement.Clean,
                 < 35 => Judgement.Fair,
@@ -22,24 +24,52 @@ namespace XanaduProject.DataStructure
             };
 
         /// <summary>
+        /// The max deviation a given judgement type can have
+        /// </summary>
+        /// <param name="judgement"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        public static double JudgementDeviation(Judgement judgement) =>
+            judgement switch
+            {
+                Judgement.FlawlessP =>  8.3,
+                Judgement.Flawless => 16,
+                Judgement.Clean => 27,
+                Judgement.Fair => 35,
+                Judgement.Deficient => 70,
+                Judgement.Terrible => 120,
+                Judgement.Miss => 200,
+                _ => throw new ArgumentOutOfRangeException(nameof(judgement), judgement, null)
+            };
+        /// <summary>
         /// Returns the text for the provided judgement.
         /// </summary>
         /// <returns></returns>
         public static string GetJudgmentText(Judgement judgement) =>
+            judgement == Judgement.FlawlessP ? "Flawless +" :  judgement.ToString();
+
+
+        /// <summary>
+        /// Returns the text for the provided judgement.
+        /// </summary>
+        /// <returns></returns>
+        public static Color GetJudgmentColor(Judgement judgement) =>
             judgement switch
             {
-                Judgement.Flawless => "Flawless",
-                Judgement.Clean => "Clean",
-                Judgement.Fair => "Fair",
-                Judgement.Deficient => "Deficient",
-                Judgement.Terrible => "Terrible",
-                Judgement.Miss => "Miss",
+                Judgement.FlawlessP => Colors.MediumPurple.Lightened(0.1f),
+                Judgement.Flawless => Colors.MediumPurple,
+                Judgement.Clean => Colors.Blue,
+                Judgement.Fair => Colors.GreenYellow,
+                Judgement.Deficient => Colors.Orange,
+                Judgement.Terrible => Colors.DarkOrange,
+                Judgement.Miss => Colors.DarkRed,
                 _ => throw new ArgumentOutOfRangeException(nameof(judgement), judgement, null)
             };
     }
 
     public enum Judgement
     {
+        FlawlessP,
         Flawless,
         Clean,
         Fair,
