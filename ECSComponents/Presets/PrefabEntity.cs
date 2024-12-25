872a5cbdfa88627a5abc64020767618d80eb6b20
mk56_spn <darioheadleysilva@gmail.com>
2024-12-25 20:09:07 +0100

    Add premade entities

--- ECSComponents/Presets/PrefabEntity.cs
+++ ECSComponents/Presets/PrefabEntity.cs
@@ -6,34 +6,34 @@ using Godot;
 
 namespace XanaduProject.ECSComponents.Presets
 {
-    public static class PrefabEntity
-    {
-        public static void Polygon(Entity entity, Vector2 pos, Vector2[] points) =>
-            entity.Add(createElement(pos), new PolygonEcs { Points = points });
+	public static class PrefabEntity
+	{
+		public static void Polygon(Entity entity, Vector2 pos, Vector2[] points) =>
+			entity.Add(createElement(pos), new PolygonEcs { Points = points });
 
-        public static void Note(Entity entity, Vector2 pos, float timing, NoteType t, World2D world)
-        {
-            ElementEcs e;
-            entity.Add(e = createElement(pos), createNote(timing,t), HitZoneEcs.Create(e, world));
-        }
+		public static void Note(Entity entity, Vector2 pos, float timing, NoteType t, World2D world)
+		{
+			ElementEcs e;
+			entity.Add(e = createElement(pos), createNote(timing,t), HitZoneEcs.Create(e, world));
+		}
 
-        public static void HoldNote(Entity entity, Vector2 pos, float timing, NoteType t, float duration, Vector2 endPosition) =>
-            entity.Add(createElement(pos), createNote(timing,t), new HoldEcs { Duration = duration, EndPosition = endPosition});
+	   /* public static void HoldNote(Entity entity, Vector2 pos, float timing, NoteType t, float duration, Vector2 endPosition) =>
+dn			entity.Add(createElement(pos), createNote(timing,t), new HoldEcs { Duration = duration, EndPosition = endPosition});*/
 
-        public static void Rect(Entity entity, Vector2 pos, Vector2 extents) =>
-            entity.Add(createElement(pos), createRect(extents));
+		public static void Rect(Entity entity, Vector2 pos, Vector2 extents) =>
+			entity.Add(createElement(pos), createRect(extents));
 
 
 
-        public static void Block(Entity entity, Vector2 pos, Vector2 extents) =>
-            entity.Add(createElement(pos), new BlockEcs(), createRect(extents));
+		public static void Block(Entity entity, Vector2 pos, Vector2 extents) =>
+			entity.Add(createElement(pos), new BlockEcs(), createRect(extents));
 
-        private static RectEcs createRect(Vector2 extents) => new() { Extents = extents, LineWidth = 6};
-        private static NoteEcs createNote(float timing, NoteType type) => new(timing,type);
-        private static ElementEcs createElement(Vector2 pos) => new()
-        {
-                Transform = Transform2D.Identity with { Origin = pos },
-                Colour = Colors.White,
-            };
-    }
+		private static RectEcs createRect(Vector2 extents) => new() { Extents = extents, LineWidth = 6};
+		private static NoteEcs createNote(float timing, NoteType type) => new(timing,type);
+		private static ElementEcs createElement(Vector2 pos) => new()
+		{
+				Transform = Transform2D.Identity with { Origin = pos },
+				Colour = Colors.White,
+			};
+	}
 }
