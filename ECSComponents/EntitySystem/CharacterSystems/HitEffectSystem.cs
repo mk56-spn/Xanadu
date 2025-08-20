3461b3da3d1c1f7489a36a9274ab7418ac18ad88
mk56_spn <darioheadleysilva@gmail.com>
2025-08-21 00:10:44 +0200

    Add bars to hit effect

--- ECSComponents/EntitySystem/CharacterSystems/HitEffectSystem.cs
+++ ECSComponents/EntitySystem/CharacterSystems/HitEffectSystem.cs
@@ -1,10 +1,12 @@
 // Copyright (c) mk56_spn <dhsjplt@gmail.com>.Licensed under the GNU General Public Licence (2.0).
 // See the LICENCE file in the repository root for full licence text.
 
+using System.Collections.Generic;
 using System.Linq;
 using Friflo.Engine.ECS;
 using Friflo.Engine.ECS.Systems;
 using Godot;
+using XanaduProject.Animation;
 using XanaduProject.Audio;
 using XanaduProject.Character;
 using XanaduProject.DataStructure;
@@ -19,6 +21,14 @@ namespace XanaduProject.ECSComponents.EntitySystem.CharacterSystems
         private readonly IVisualsMaster visualsMaster = DiProvider.Get<IVisualsMaster>();
         private readonly IClock clock = DiProvider.Get<IClock>();
 
+        public HitEffectSystem()
+        {
+            clock.Stopped += () => {
+                foreach (var canvases in effectsRef)
+                    RenderingServer.FreeRid(canvases);
+                effectsRef.Clear();
+            };
+        }
         protected override void OnUpdate()
         {
             Query.ForEachEntity(((ref CharacterEcs character, Entity _) =>
@@ -32,20 +42,39 @@ namespace XanaduProject.ECSComponents.EntitySystem.CharacterSystems
             }));
         }
 
+        private readonly AnimationMaterial animationMaterial = new();
+
+
+        private readonly List<RenderRid> effectsRef = new(300);
         private void setupHitVisuals(NoteType type, Vector2 pos)
         {
-           RenderRid rid = RenderRid.Create(visualsMaster.GameplayerLayerRid, 0.7f)
-                .SetZIndex(2)
-                .SetTransform(new Transform2D(0, pos))
-                .SetModulate(type.NoteColor())
+            const float duration = 1f;
+
+            RenderRid rid = setupRid()
                 .AddRect(new Vector2(100, 100))
                 .SetMaterial(Materials.HITS.Get(HitShaderId.Default));
 
-            InstanceShaders.SetHitTime(rid, (float)clock.PlaybackTimeSec);
+            RenderRid line = setupRid()
+                .AddRect(new Vector2(10, 3000))
+                .SetMaterial(animationMaterial.GetRid());
 
-            if (type == NoteType.C) RenderingServer.CanvasItemSetInstanceShaderParameter(rid, "y_rot", 35f);
+           animationMaterial.SetAnimationDuration(line,duration);
+           animationMaterial.SetAnimationStartCurrent(line);
+           animationMaterial.SetScaled(line,Vector2.Down);
 
-            clock.Stopped += () => RenderingServer.FreeRid(rid);
+           effectsRef.Add(rid);
+           effectsRef.Add(line);
+
+           InstanceShaders.SetHitTime(rid, (float)clock.PlaybackTimeSec);
+           if (type == NoteType.C) RenderingServer.CanvasItemSetInstanceShaderParameter(rid, "y_rot", 35f);
+           clock.Stopped += () => RenderingServer.FreeRid(rid);
+
+
+           RenderRid setupRid()=>
+                RenderRid.Create(visualsMaster.GameplayerLayerRid, duration)
+                   .SetZIndex(100)
+                   .SetTransform(new Transform2D(0, pos))
+                   .SetModulate(type.NoteColor());
         }
     }
 }
