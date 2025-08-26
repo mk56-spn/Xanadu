d008a4fb42103f58f1a040d5e1497d69f3605252
mk56_spn <darioheadleysilva@gmail.com>
2025-08-26 02:45:02 +0200

    Add FontSource.cs

---
+++ FontSource.cs
@@ -0,0 +1,13 @@
+// Copyright (c) mk56_spn <dhsjplt@gmail.com>.Licensed under the GNU General Public Licence (2.0).
+// See the LICENCE file in the repository root for full licence text.
+
+using Godot;
+
+namespace XanaduProject
+{
+    public static class FontSource
+    {
+        public static readonly Font LINE_THIN = GD.Load<Font>("uid://dghg2sit48qtn");
+        public static readonly Font PLASTIC = GD.Load<Font>("uid://ehrnfq107vtu");
+    }
+}
