4243723969c406709853a9fe064bd2c9634dafac
mk56_spn <darioheadleysilva@gmail.com>
2025-08-27 01:29:25 +0200

    Add a class for UI materials

---
+++ Screens/UiMaterials.cs
@@ -0,0 +1,13 @@
+// Copyright (c) mk56_spn <dhsjplt@gmail.com>.Licensed under the GNU General Public Licence (2.0).
+// See the LICENCE file in the repository root for full licence text.
+
+using Godot;
+
+namespace XanaduProject.Screens
+{
+    public static class UiMaterials
+    {
+        public static readonly ShaderMaterial FLARE = new() {
+            Shader = GD.Load<Shader>("uid://rdmyubghet5p") };
+    }
+}
