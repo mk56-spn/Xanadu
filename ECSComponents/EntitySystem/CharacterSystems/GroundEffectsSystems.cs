1ba9ba62c197579b00abf481022158fd2cdfbd88
mk56_spn <darioheadleysilva@gmail.com>
2025-09-01 16:21:07 +0200

    Update nesting in GroundEffectsSystems.cs

--- ECSComponents/EntitySystem/CharacterSystems/GroundEffectsSystems.cs
+++ ECSComponents/EntitySystem/CharacterSystems/GroundEffectsSystems.cs
@@ -54,15 +54,15 @@ namespace XanaduProject.ECSComponents.EntitySystem.CharacterSystems
 
 
 			store.Query<CharacterEcs>().Entities.First().AddSignalHandler<Airborne>(_ =>
-            {
-                groundParticles.SetAmountRatio(0);
-            });
+			{
+				groundParticles.SetAmountRatio(0);
+			});
 		}
 
 		protected override void OnUpdate()
 		{
 			Transform2D xf2d = Transform2D.Identity;
-			Query.ForEachEntity((ref CharacterEcs characterEcs, Entity entity) =>
+			Query.ForEachEntity((ref CharacterEcs characterEcs, Entity _) =>
 			{
 				if (characterEcs.Phase == Phase.Airborne) return;
 				groundParticles.SetAmountRatio(Mathf.Abs(characterEcs.Velocity.X / PlayerCharacter.MAX_RUN_SPEED));
