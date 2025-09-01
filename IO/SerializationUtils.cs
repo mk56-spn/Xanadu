372f613ccf9e47c3b7f5fa282a310f40bdc15584
mk56_spn <darioheadleysilva@gmail.com>
2025-09-01 23:20:17 +0200

    Add common SerializationUtils.cs to single file for reuse

---
+++ IO/SerializationUtils.cs
@@ -0,0 +1,13 @@
+// Copyright (c) mk56_spn <dhsjplt@gmail.com>. Licensed under the GNU General Public Licence (2.0).
+// See the LICENCE file in the repository root for full licence text.
+
+namespace XanaduProject.IO
+{
+    internal static class SerializationUtils
+    {
+        // Loose strings are the enemy!
+        internal const string STAGES_DIR = "res://Stages";
+        internal const string METADATA_FILENAME = "metadata.json";
+        internal const string ENTITY_STORE_FILENAME = "entity_store.json";
+    }
+}
