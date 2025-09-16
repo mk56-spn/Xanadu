f0083a23986d4ad31dbe78fb9dc6ee443bfc2cdb
mk56_spn <darioheadleysilva@gmail.com>
2025-09-17 00:10:23 +0200

    usingstuff

--- ECSComponents/Interfaces/IUpdatable.cs
+++ ECSComponents/Interfaces/IUpdatable.cs
@@ -1,6 +1,8 @@
 // Copyright (c) mk56_spn <dhsjplt@gmail.com>. Licensed under the GNU General Public Licence (2.0).
 // See the LICENCE file in the repository root for full licence text.
 
+using XanaduProject.ECSComponents.EntitySystem.Components;
+
 namespace XanaduProject.ECSComponents.Interfaces
 {
     internal interface IUpdatable
