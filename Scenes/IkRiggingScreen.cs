ea4fa93c96127c6b0f8439bd93360ae97f0881a5
mk56_spn <darioheadleysilva@gmail.com>
2025-09-18 01:49:59 +0200

    Change ik riggingscreen

--- Scenes/IkRiggingScreen.cs
+++ Scenes/IkRiggingScreen.cs
@@ -6,11 +6,16 @@ using Friflo.Engine.ECS.Systems;
 using Godot;
 using Microsoft.Extensions.DependencyInjection;
 using Xanadu.Singletons;
+using XanaduProject.Buttons;
+using XanaduProject.ECSComponents.EntitySystem;
 using XanaduProject.ECSComponents.EntitySystem.BoneSystems;
 using XanaduProject.ECSComponents.EntitySystem.Components.Bones;
 using XanaduProject.Factories;
 using XanaduProject.GameDependencies;
+using XanaduProject.Scenes.MeshEditor;
 using XanaduProject.Screens.ScreenStructure;
+using XanaduProject.IO.Indexes; // Added for ItemIndex
+using XanaduProject.IO; // Added for Item
 using Logger = XanaduProject.Singleton.Logger;
 
 namespace XanaduProject.Scenes
@@ -25,26 +30,90 @@ namespace XanaduProject.Scenes
 
         public IkRiggingScreen()
         {
-            DiProvider.Register(c=>
-                c.AddSingleton(entityStore));
-
+            DiProvider.Register(c =>
+            {
+                c.AddSingleton(entityStore);
+            });
             PoseBuilder.BuildPose();
+
 
             simulationRoot = new SystemRoot(entityStore)
             {
                 new BoneTransformSystem(),
                 new IkSolverSystem(),
                 new BoneRenderingSystem(),
-              /*  new SkinRenderingSystem2D()*/
+                new EcsDebugSystem(),
             };
 
             entityStore.Query<RootEcs>().ForEachEntity((ref RootEcs rootEcs, Entity entity) =>
             {
-               rootEcs.Canvas.SetTransform(new Transform2D(0, new Vector2(200,200)));
-               rootEcs.Canvas.SetParent(GetCanvasItem());
+                rootEcs.Canvas.SetTransform(new Transform2D(0, new Vector2(200, 200)));
+                rootEcs.Canvas.SetParent(GetCanvasItem()).SetZIndex(100);
             });
 
-            Logger.AddLog(LogCategory.Animation, "Ik rigging screen initialized");
+            setupUiLayout();
+
+            Logger.AddLog(LogCategory.Animation, "Ik rigging screen with mesh editor initialized");
+        }
+
+
+        private void setupUiLayout()
+        {
+            var menuContainer = new VBoxContainer();
+            AddChild(menuContainer);
+
+            var poseButton = new AnimatedHoverButton("Pose Animating", 15);
+            menuContainer.AddChild(poseButton);
+            poseButton.Pressed += () => ScreenManager.ChangeSubScreen(new PoseAnimatingSubScreen());
+
+            // --- Item Selection UI ---
+            var itemSelectionContainer = new VBoxContainer();
+            itemSelectionContainer.Name = "ItemSelectionContainer";
+            menuContainer.AddChild(itemSelectionContainer);
+
+            var createNewMeshButton = new Button { Text = "Create New Mesh" };
+            itemSelectionContainer.AddChild(createNewMeshButton);
+            createNewMeshButton.Pressed += () => ScreenManager.ChangeSubScreen(new MeshCreationSubScreen(null)); // Pass null for new item
+
+            var scrollContainer = new ScrollContainer() { CustomMinimumSize = new Vector2(500, 100)};
+            scrollContainer.SizeFlagsHorizontal = SizeFlags.ExpandFill;
+            scrollContainer.SizeFlagsVertical = SizeFlags.ExpandFill;
+            itemSelectionContainer.AddChild(scrollContainer);
+
+            var itemListContainer = new VBoxContainer();
+            itemListContainer.Name = "ItemListContainer";
+            scrollContainer.AddChild(itemListContainer);
+
+            AddChild(itemListContainer);
+
+
+            foreach (var itemInfoEntry in ItemIndex.GetAllItemInfos())
+            {
+                Logger.AddLog(LogCategory.General, $"ItemIndex: Loaded item '{itemInfoEntry.Key}' from '{itemInfoEntry.Value.FilePath}'");
+                var itemInfo = itemInfoEntry.Value;
+                var itemButton = new Button
+                {
+                    Text = itemInfo.Name,
+                    TooltipText = itemInfo.Description
+                };
+                itemButton.Pressed += () =>
+                {
+                    Item? selectedItem = ItemIndex.GetItem(itemInfo.Name);
+                    if (selectedItem != null)
+                    {
+                        ScreenManager.ChangeSubScreen(new MeshCreationSubScreen(selectedItem));
+                    }
+                    else
+                    {
+                        GD.PrintErr($"Failed to load item: {itemInfo.Name}");
+                    }
+                };
+                itemListContainer.AddChild(itemButton);
+
+            }
+            itemListContainer.AddChild(new AnimatedHoverButton("lmo"));
+
+            // --- End Item Selection UI ---
         }
 
         public override void _Process(double delta)
